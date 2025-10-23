f054896eaff148f5c681461ac9755ea2d8cab722
Omar Alispahic <omaralispahic@MacBookPro.home>
2025-10-24 00:21:16 +0200

    Dodana osnovna korisnik klasa

---
+++ upravljanje_korisnicima.cs
@@ -0,0 +1,24 @@
+using System;
+
+public enum Uloga
+{
+    Administrator,
+    Student,
+    Profesor,
+}
+
+public class Korisnik
+{
+    string []uloge = { "Administrator", "Student", "Profesor" };    
+    public string Ime;
+    public string Prezime;
+    public Uloga KorisnickaUloga;
+
+    public Korisnik(string ime, string prezime, Uloga uloga)
+    {
+        this.Ime = ime;
+        this.Prezime = prezime;
+        this.KorisnickaUloga = uloga;
+
+    }
+}
