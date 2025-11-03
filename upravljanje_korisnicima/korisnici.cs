5d20ea7b801afb6632272b4c767785cabee1dfbb
Omar Alispahic <omaralispahic@MacBookPro.home>
2025-11-03 19:03:08 +0100

    Manje izmjene ok korisnickih ID brojeva

--- upravljanje_korisnicima/korisnici.cs
+++ upravljanje_korisnicima/korisnici.cs
@@ -1,7 +1,7 @@
 using System;
-using System.Text.Json;
 using System.IO;
 using System.Linq;
+using System.Text.Json;
 
 public enum Uloge
 {
@@ -14,10 +14,10 @@ public class Korisnik
 {
     string[] uloge = { "Administrator", "Student", "Profesor" };
 
-    private static int lastAssignedId = 0;
+    public static int lastAssignedId = 0;
     public string Ime { get; private set; }
     public string Prezime { get; private set; }
-    public Uloge Uloga { get; private set; }
+    public Uloge Uloga { get; set; }
 
     public int userID { get; private set; }
 
@@ -27,8 +27,51 @@ public class Korisnik
         this.Prezime = prezime;
         this.Uloga = uloga;
         this.userID = ++lastAssignedId;
+    }
+}
 
+/*
+using System;
+using System.Text.Json.Serialization;
+
+public enum Uloge
+{
+    Administrator,
+    Student,
+    Profesor,
+}
+
+public class Korisnik
+{
+    private static int lastAssignedId = 0;
+
+    [JsonInclude]
+    public string Ime { get; private set; }
+
+    [JsonInclude]
+    public string Prezime { get; private set; }
+
+    [JsonInclude]
+    public Uloge Uloga { get; set; } // now public setter ✅
+
+    [JsonInclude]
+    public int userID { get; private set; }
+
+    public Korisnik(string ime, string prezime, Uloge uloga)
+    {
+        this.Ime = ime;
+        this.Prezime = prezime;
+        this.Uloga = uloga;
+        this.userID = ++lastAssignedId;
     }
 
+    // Needed for JSON deserialization
+    public Korisnik() { }
 
+    public static void SetLastAssignedId(int value)
+    {
+        if (value > lastAssignedId)
+            lastAssignedId = value;
+    }
 }
+*/
