aecc69129b8770ec5151f1f015642d01aecf618d
BakirH1 <bhadzialic1@etf.unsa.ba>
2025-11-08 18:48:11 +0100

    Bug fixes

--- obj/Debug/net9.0/LibraryTS.AssemblyInfo.cs
+++ obj/Debug/net9.0/LibraryTS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryTS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5eae756ae7c8cb125dc7c36fed4cd181a3afcd1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c869b3d7155622a4adbb94958b18a9b1c68920f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryTS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryTS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
